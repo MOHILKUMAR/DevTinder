454f3cfeb8e9b8c1d1f49045783f6d2339dd9656
user.Mohil Kumar <user.mohil4280@gmail.com>
2025-11-11 02:11:12 +0530

    build the user card on the feed

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const UserCard = ({user} ) => {
+    // console.log(user);
+    const {firstName, lastName,photoUrl, age, gender, about } = user;
+  return (
+    <div>
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <figure>
+          <img
+            src={photoUrl}
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{firstName + ' ' + lastName}</h2>
+          {age && gender && <p>{age + " , " + gender}</p>}
+          <p>
+           {about}
+          </p>
+          <div className="card-actions justify-center my-4">
+            <button className="btn btn-primary">Ingore</button>
+            <button className="btn  bg-green-400 border border-green-400">Interested</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
